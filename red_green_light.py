ab1d15a5114161a845bb31d5c803b78ed07388a1
Florin Barbisch <40719099+florinbarbisch@users.noreply.github.com>
2022-09-20 19:10:25 +0200

    Added: test program for jürg lüthiger

---
+++ red_green_light.py
@@ -0,0 +1,27 @@
+# Write your code here :-)
+import time
+import board
+import analogio
+import digitalio
+import adafruit_dht
+import adafruit_scd30
+from rgbled import ChainableLED
+
+
+# Light
+# A0 auf Shield und Feather
+A0 = analogio.AnalogIn(board.A0)
+ 
+
+# RGB LED
+# A2(D16) auf Shield und Feather
+A2 = ChainableLED(board.A2, board.A3, 1)
+
+
+while True:
+    LIGHT = int(A0.value)
+    light_to_scale = LIGHT* 255//42_000 
+    A2.setColorRGB(light_to_scale, 255-light_to_scale,   0)
+    print((1, LIGHT))
+   
+    time.sleep(0.01)
